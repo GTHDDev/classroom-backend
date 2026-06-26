fdaf400c635698838afe125a112e7d9c45bfb7a3
Miguel Eduardo Ovando Alvarez <miguel-workspace@outlook.com>
2026-06-26 07:22:18 -0600

    feat: Implement subjects API with CORS support.
    Added a new subjects route for fetching subject data with pagination and filtering options. Integrated CORS middleware to allow cross-origin requests. Updated package dependencies to include 'cors' and its type definitions.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import express from 'express';
+import cors from 'cors';
+import SubjectRouter from './routes/subjects';
 
 const app = express();
 const PORT = 8000;
 
+app.use(express.json());
+
+app.use('/api/subjects', SubjectRouter);
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  }),
+);
+
 app.get('/', (req, res) => {
-  res.send('Hello, this is the classroom API');
+  res.send('Hello, this  the classroom API');
 });
 
 app.listen(PORT, () => {

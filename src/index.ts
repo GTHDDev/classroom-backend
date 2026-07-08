e827ea4d0b420edc730458dd3978ca3e04bae873
Miguel Eduardo Ovando Alvarez <miguel-workspace@outlook.com>
2026-07-08 14:28:16 -0600

    feat: enhance API with new routes and class management
    - Add new routes for classes, subjects, and users
      with pagination and filtering capabilities
    - Implement class creation and retrieval
      including detailed information
    - Enhance subjects API for creation and
      detailed retrieval with associated classes
    - Introduce user management routes for
      fetching details, departments, and subjects
    - Update index.ts to integrate new routers
      and improve APM error handling

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,26 @@
-import AgentApi from 'apminsight';
-import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
-import subjectsRouter from './routes/subjects.js';
-import securityMiddleware from './middleware/security.js';
-import { toNodeHandler } from 'better-auth/node';
-import { auth } from './lib/auth.js';
+import('apminsight')
+  .then(({ default: AgentAPI }) => AgentAPI.config())
+  .catch(() => console.log('APM not available in this environment'));
 
-AgentApi.config();
+import cors from 'cors';
+import express from 'express';
+import { toNodeHandler } from 'better-auth/node';
+
+import subjectsRouter from './routes/subjects.js';
+import usersRouter from './routes/users.js';
+import classesRouter from './routes/classes.js';
+
+// import securityMiddleware from "./middleware/security.js";
+import { auth } from './lib/auth.js';
 
 const app = express();
 const PORT = 8000;
 
-const frontendOrigin = process.env.FRONTEND_URL?.replace(/\/$/, '');
-
 app.use(
   cors({
-    origin: frontendOrigin,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
+    origin: process.env.FRONTEND_URL, // React app URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+    credentials: true, // allow cookies
   }),
 );
 
@@ -26,9 +28,11 @@ app.all('/api/auth/*splat', toNodeHandler(auth));
 
 app.use(express.json());
 
-app.use(securityMiddleware);
+// app.use(securityMiddleware);
 
 app.use('/api/subjects', subjectsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/classes', classesRouter);
 
 app.get('/', (req, res) => {
   res.send('Backend server is running!');
